934c314536d69c4b52d7378652ed7cac5d85f71c
Dannie Vinther <davi@kea.dk>
2025-02-25 10:04:03 +0100

    Enhance Astro configuration with prefetch settings and client prerendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
   experimental: {
     svg: true,
+    clientPrerender: true,
   },
 });
